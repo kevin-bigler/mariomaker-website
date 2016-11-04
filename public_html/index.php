35d1cbfd2d781e625ffd11939c6a737f39b15ff6
Kevin Bigler <kevinbigler@gmail.com>
2016-11-04 02:59:49 +0000

    Add a few things to start off, like adminer, a composer library, and some starting Slim PHP index and htaccess code.

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,19 @@
 <?php
 
-echo 'hello world of mm';
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 
-// TODO do a file get contents or w/e to get my profile page and level codes, links
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../composer/vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
